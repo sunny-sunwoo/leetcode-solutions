4f318a9da3494d9ea6de9529a65233f0baa9eb2a
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-06-30 23:27:00 -0700

    add test for lc92

--- LC92_ReverseLL2.java
+++ LC92_ReverseLL2.java
@@ -27,6 +27,7 @@ public class LC92_ReverseLL2 {
         
         while (stride != null && cnt < n - m) {
             stride = stride.next;
+            cnt++;
         }
         Node remaining = stride.next;
         prev.next = null;
@@ -55,5 +56,30 @@ public class LC92_ReverseLL2 {
     
     private static class Node {
         Node next;
+        int val;
+        public Node(int val) {
+            this.val = val;
+        }
+        
+        @Override
+        public String toString() {
+            if (next == null) return String.valueOf(val);
+            return String.valueOf(val) +  " -> " + next.toString();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        
+        reverseLL_pointers(n1, 2, 4);
+        System.out.println(n1);
     }
 }
